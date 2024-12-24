6d17895b55f67f66aebbc0b631f9e5cfc68dffd6
gauravgit config --global user.emailgit config --global user.name gaurav <gaurav.kashyap@sachtechsolution.com>
2024-12-24 12:28:25 +0530

    mongo chnages

--- src/configs/mongoose.config.ts
+++ src/configs/mongoose.config.ts
@@ -1,12 +1,22 @@
 import mongoose from "mongoose";
 import { DB } from "../constants/app.constant";
+import dotenv from "dotenv";
+import path from "path";
+const envConfig = dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+if (envConfig.error) {
+  throw new Error("No .Env File Found");
+}
 
 export const connection = async () => {
 
-  const MONGO_URI = 'mongodb://i6eR_Dev:i6eR_1077@solidappmaker.in/i6eR_Dev'
-  // const MONGO_URI = 'mongodb://i6eR_Dev:i6eR_1077@localhost/i6eR_Dev'
+  const LOCAL_MONGO_URI = 'mongodb://i6eR_Dev:i6eR_1077@223.178.220.83/i6eR_Dev'
+  const SERVER_MONGO_URI = 'mongodb://i6eR_Dev:i6eR_1077@localhost/i6eR_Dev'
+
+  const MONGO_URI = process.env.ENV_MODE === 'DEV' ? SERVER_MONGO_URI : LOCAL_MONGO_URI;
+
   // const MONGO_URI = await DB.MONGODB_URI
-  console.log(MONGO_URI, "MONGO_URI_CONNECTION_SIDEE")
+  console.log(MONGO_URI, "URI_MONGO_URI_CONNECTION_SIDEE")
   mongoose.Promise = global.Promise;
 
   await mongoose.connect(MONGO_URI as string, {} as mongoose.ConnectOptions)
